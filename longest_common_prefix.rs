362c58f7fce9f77fe7b12df61b7d353f75e6f855
verghil <verghil2012@yandex.ru>
2025-10-21 23:44:57 +0300

    new problem with test

--- longest_common_prefix.rs
+++ longest_common_prefix.rs
@@ -1,20 +1,21 @@
+
+
 pub fn longest_common_prefix(strs: Vec<String>) -> String{
 	let mut aux: String = String::new();
 	let mut answer: String = String::new();
-
 	if strs.iter().len() != 1{
-
 		for (word_index, word) in strs.iter().enumerate(){
-			
 			if aux != "" {
-
-				for (letter_index, letters) in word.chars().enumerate(){
-					if letters == aux.chars().nth(letter_index).unwrap(){
-						answer.push(letters);
-					}else{
+				for (letter_index, letter) in word.chars().enumerate(){
+					
+					if letter_index >= aux.len(){
 						break;
 					};
-				}
+						match letter{
+							letter if letter == aux.chars().nth(letter_index).unwrap() => answer.push(letter),
+							_ => break,
+						};
+				};
 
 				if word_index != strs.iter().len() - 1{
 					aux = answer.clone();
@@ -54,5 +55,10 @@ fn main(){
 	let strs:Vec<String> = vec!["aaa".to_string(),"aa".to_string(),"aaa".to_string()];
 	let result = longest_common_prefix(strs);
 	println!("Ещё один тест {}", result);
+	// Проблема
+	let strs:Vec<String> = vec!["caa".to_string(),"".to_string(),"a".to_string(),"acb".to_string()];
+	let result = longest_common_prefix(strs);
+	println!("Ещё один тест {}", result);
+
 
 }
