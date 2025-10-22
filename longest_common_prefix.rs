9fd35f2bb36b23ae71ff8238db8d1d9d8c9e0e64
verghil <verghil2012@yandex.ru>
2025-10-22 23:28:25 +0300

    longest done

--- longest_common_prefix.rs
+++ longest_common_prefix.rs
@@ -3,37 +3,43 @@
 pub fn longest_common_prefix(strs: Vec<String>) -> String{
 	let mut aux: String = String::new();
 	let mut answer: String = String::new();
-	if strs.iter().len() != 1{
-		for (word_index, word) in strs.iter().enumerate(){
-			if aux != "" {
-				for (letter_index, letter) in word.chars().enumerate(){
-					
-					if letter_index >= aux.len(){
-						break;
-					};
-						match letter{
-							letter if letter == aux.chars().nth(letter_index).unwrap() => answer.push(letter),
-							_ => break,
+	let mut flag: bool = false;
+	
+	match strs {
+		ref strs_vec if strs_vec.iter().len() == 1 => answer = strs.iter().next().unwrap().clone(),
+		ref strs_vec if strs_vec[0].chars().next() != strs_vec[1].chars().next() => answer = "".to_string(),
+		_ =>
+			for (word_index, word) in strs.iter().enumerate(){
+				if aux != "" {
+					for (letter_index, letter) in word.chars().enumerate(){
+						
+						if letter_index >= aux.len(){
+							break;
 						};
-				};
+							match letter{
+								letter if letter == aux.chars().nth(letter_index).unwrap() => answer.push(letter),
+								_ => break,
+							};
+					};
 
-				if word_index != strs.iter().len() - 1{
-					aux = answer.clone();
-					answer = "".to_string();
+					if word_index != strs.iter().len() - 1{
+						aux = answer.clone();
+						answer = "".to_string();
+
+					}else{
+						continue;
+					};
+
 
 				}else{
-					continue;
+					if flag == true{
+						break;
+					}
+					aux = word.clone();
+					flag = true;
 				};
-
-
-			}else{
-				aux = word.clone();
-			};
-
+			}
 		}
-	}else{
-		answer = strs.iter().next().unwrap().clone();
-	};
 		answer
 
 }
@@ -42,23 +48,38 @@ pub fn longest_common_prefix(strs: Vec<String>) -> String{
 fn main(){
 	let strs: Vec<String> = vec!["flower".to_string(),"flow".to_string(),"flight".to_string()];
 	let result = longest_common_prefix(strs);
+	println!("Должен быть fl");
 	println!("результат {}", result);
+	println!("");
 
 	let strs:Vec<String> = vec!["dog".to_string(),"racecar".to_string(),"car".to_string()];
 	let result = longest_common_prefix(strs);
+	println!("Должен быть ()");
 	println!("Ещё один тест {}", result);
+	println!("");
 
 	let strs:Vec<String> = vec!["a".to_string()];
 	let result = longest_common_prefix(strs);
+	println!("Должен быть a");
 	println!("Ещё один тест {}", result);
+	println!("");
 
 	let strs:Vec<String> = vec!["aaa".to_string(),"aa".to_string(),"aaa".to_string()];
 	let result = longest_common_prefix(strs);
+	println!("Должен быть aa");
 	println!("Ещё один тест {}", result);
+	println!("");
 	// Проблема
 	let strs:Vec<String> = vec!["caa".to_string(),"".to_string(),"a".to_string(),"acb".to_string()];
 	let result = longest_common_prefix(strs);
+	println!("Должен быть ()");
 	println!("Ещё один тест {}", result);
+	println!("");
 
+	
+	let strs:Vec<String> = vec!["ca".to_string(),"c".to_string(),"bba".to_string(),"bacb".to_string(),"bcb".to_string()];
+	let result = longest_common_prefix(strs);
+	println!("Должен быть ()");
+	println!("Ещё один тест {}", result);
 
 }
