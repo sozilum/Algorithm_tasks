092b7ecb7a25c7d3fa5a89f9857583c93427d280
root <root@localhost.localdomain>
2025-10-29 16:52:56 +0000

    begin task, merge 2 sorted linkedlist

---
+++ merge_to_list.rs
@@ -0,0 +1,47 @@
+use std::collections::HasMap;
+
+impl Solution{
+	pub fn merge_to_list(list1: Option<Box<ListNode>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
+		#[derive(Debug)]
+		pub struct ListNode {
+			val: i32,
+			next: Option<Box<ListNode>>
+		}
+
+		impl ListNode{
+			fn new(val: i32) -> Self{
+				ListNode {val, next: None}
+			}
+
+			fn append(&mut self, val: i32: ){
+				let mut current = self;
+
+				while let Some(ref mut next) = current.next {
+					current = next;
+				}
+
+				current.next = Some(Box::new(ListNode::new(val)));
+			}
+		}
+
+		pub fn iter_to_linked_list(list: Option<Box<ListNode>>,&mut hashmap: HashMap<i32, i32>) -> {
+			while let Some(list) = current {
+
+			match list.get(&list){
+				Some(&num) => list.get(&num) += 1,
+				_ => list.insert(num),
+			}
+			
+			current = &list.next;	
+			}
+		}
+		let mut integer_hashmap: HashMap<i32, i32> = HashMap::new();
+
+		_ = iter_to_linked_list(list1, &integer_hashmap);
+		_ = iter_to_linked_list(list2, &integer_hashmap);
+
+		for num in ingete_hashmap.len(){}
+				
+	};
+	
+}
