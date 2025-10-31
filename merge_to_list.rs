7ac76112c13979b004b84c2e36650e3935d52cd2
verghil <verghil2012@yandex.ru>
2025-10-31 23:18:10 +0300

    merge to sorted list is done

--- merge_to_list.rs
+++ merge_to_list.rs
@@ -1,66 +1,54 @@
+#[derive(Debug)]
+pub struct ListNode {
+	pub val: i32,
+	pub next: Option<Box<ListNode>>
+}
 
-fn main(){
-	use std::thread::current;
-	use std::collections::HashMap;
+impl ListNode{
+	fn new(val: i32) -> Self{
+		ListNode{val, next: None}
+	}
 
-	impl<ListNode>  Solution{
-		pub fn merge_to_list(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
-			#[derive(Debug)]
-			pub struct ListNode {
-				val: i32,
-				next: Option<Box<ListNode>>
-			}
+	fn append(&mut self, val: i32){
+		let mut current = self;
 
-			impl ListNode{
-				fn new(val: i32) -> Self{
-					ListNode {val, next: None}
-				}
-
-				fn append(&mut self, val: i32){
-					let mut current = self;
-
-					while let Some(ref mut next) = current.next {
-						current = next;
-					}
-
-					current.next = Some(Box::new(ListNode::new(val)));
-				}
-			}
-
-			pub fn iter_to_linked_list(list: Option<Box<ListNode>>,&mut hashmap:&mut  HashMap<i32, i32>){
-				while let Some(list) = current {
-
-				match list.get(&list){
-					Some(&num) => list.get(&num) += 1,
-					_ => list.insert(num),
-				}
-				
-				}
-				let current = &list.unwrap();	
-			}
-			let mut integer_hashmap: HashMap<i32, i32> = HashMap::new();
-			let mut list_node: ListNode = ListNode::new(<i32>);
-			_ = iter_to_linked_list(list1, &integer_hashmap);
-			_ = iter_to_linked_list(list2, &integer_hashmap);
-
-
-			for keys in integer_hashmap.keys()(){
-
-				for num in integer_hashmap.get(keys){
-
-					list_node.append(*keys);
-					
-				}
-			}
-		list_node	
+		while let Some(ref mut next) = current.next {
+			current = next;
 		}
+
+		current.next = Some(Box::new(ListNode::new(val)));
 	}
 }
 
+fn main(){
+	
+	pub fn merge_to_list(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
+			
+			pub fn iter_to_linked_list(list: Option<Box<ListNode>>, integer_vector:&mut Vec<i32>){
+				let mut current = list;	
+			
+				while let Some(node) = current {
+					let num = node.val;
+					integer_vector.push(num);
+					current = node.next;
+				}
+			}
+			let mut integer_vector: Vec<i32> = vec![];
+			iter_to_linked_list(list1, &mut integer_vector);
+			iter_to_linked_list(list2, &mut integer_vector);
+			
+			if integer_vector.len() == 0{
+				return None
+			};
+			
+			integer_vector.sort();
+			let first_num = integer_vector.remove(0);
+			let mut list_node: ListNode = ListNode::new(first_num);
 
 
-
-
-
-
-
+			for num in integer_vector.iter(){
+				list_node.append(*num);
+			}
+		Some(Box::new(list_node))
+	}
+}
