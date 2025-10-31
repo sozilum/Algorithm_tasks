b9b50eec22ac0ea44f55cf1196fba09ffbefc25f
root <root@localhost.localdomain>
2025-10-31 07:45:53 +0000

    lil progres, main logic is done

--- merge_to_list.rs
+++ merge_to_list.rs
@@ -1,47 +1,66 @@
-use std::collections::HasMap;
 
-impl Solution{
-	pub fn merge_to_list(list1: Option<Box<ListNode>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
-		#[derive(Debug)]
-		pub struct ListNode {
-			val: i32,
-			next: Option<Box<ListNode>>
-		}
+fn main(){
+	use std::thread::current;
+	use std::collections::HashMap;
 
-		impl ListNode{
-			fn new(val: i32) -> Self{
-				ListNode {val, next: None}
+	impl<ListNode>  Solution{
+		pub fn merge_to_list(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
+			#[derive(Debug)]
+			pub struct ListNode {
+				val: i32,
+				next: Option<Box<ListNode>>
 			}
 
-			fn append(&mut self, val: i32: ){
-				let mut current = self;
-
-				while let Some(ref mut next) = current.next {
-					current = next;
+			impl ListNode{
+				fn new(val: i32) -> Self{
+					ListNode {val, next: None}
 				}
 
-				current.next = Some(Box::new(ListNode::new(val)));
+				fn append(&mut self, val: i32){
+					let mut current = self;
+
+					while let Some(ref mut next) = current.next {
+						current = next;
+					}
+
+					current.next = Some(Box::new(ListNode::new(val)));
+				}
 			}
-		}
 
-		pub fn iter_to_linked_list(list: Option<Box<ListNode>>,&mut hashmap: HashMap<i32, i32>) -> {
-			while let Some(list) = current {
+			pub fn iter_to_linked_list(list: Option<Box<ListNode>>,&mut hashmap:&mut  HashMap<i32, i32>){
+				while let Some(list) = current {
 
-			match list.get(&list){
-				Some(&num) => list.get(&num) += 1,
-				_ => list.insert(num),
-			}
-			
-			current = &list.next;	
-			}
-		}
-		let mut integer_hashmap: HashMap<i32, i32> = HashMap::new();
-
-		_ = iter_to_linked_list(list1, &integer_hashmap);
-		_ = iter_to_linked_list(list2, &integer_hashmap);
-
-		for num in ingete_hashmap.len(){}
+				match list.get(&list){
+					Some(&num) => list.get(&num) += 1,
+					_ => list.insert(num),
+				}
 				
-	};
-	
+				}
+				let current = &list.unwrap();	
+			}
+			let mut integer_hashmap: HashMap<i32, i32> = HashMap::new();
+			let mut list_node: ListNode = ListNode::new(<i32>);
+			_ = iter_to_linked_list(list1, &integer_hashmap);
+			_ = iter_to_linked_list(list2, &integer_hashmap);
+
+
+			for keys in integer_hashmap.keys()(){
+
+				for num in integer_hashmap.get(keys){
+
+					list_node.append(*keys);
+					
+				}
+			}
+		list_node	
+		}
+	}
 }
+
+
+
+
+
+
+
+
