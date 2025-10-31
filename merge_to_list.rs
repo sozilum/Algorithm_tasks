b36322161c5832979a49ad9e502370ebb621e2e9
verghil <verghil2012@yandex.ru>
2025-10-31 23:22:03 +0300

    added reference

--- merge_to_list.rs
+++ merge_to_list.rs
@@ -52,3 +52,38 @@ fn main(){
 		Some(Box::new(list_node))
 	}
 }
+
+
+// The reference solution 
+
+impl Solution {
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+        // Dummy node
+        // tail = dummy
+        // while l1 and l2: 
+            // if l1.val < l2.val:
+                // Calling Recursivley
+                // append l1
+            // else 
+                // Calling Recursivley
+                // append l2
+        // appened remaining nodes
+        // return dummy.next
+        match (list1, list2) {
+            (None, None) => None,
+            (Some(n), None) | (None, Some(n)) => Some(n),
+            (Some(mut n1), Some(mut n2)) => {
+                if n1.val < n2.val {
+                    let next = Self::merge_two_lists(n1.next.take(), Some(n2));
+                    n1.next = next;
+                    Some(n1)
+                } else {
+                    let next = Self::merge_two_lists(Some(n1), n2.next.take());
+                    n2.next = next;
+                    Some(n2)
+                }
+            }
+        }
+    } 
+}
