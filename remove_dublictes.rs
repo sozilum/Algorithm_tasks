3e4f0a9bfe7b8c5e46246451d08a27f4d26953e4
verghil <verghil2012@yandex.ru>
2025-11-04 17:20:13 +0300

    remove is done

--- remove_dublictes.rs
+++ remove_dublictes.rs
@@ -1,15 +1,46 @@
-use std::collection::HashMap;
 
 fn main(){
-	pub fn remove_dublicates(&mut nums: Vec<i32>) -> i32{
-		let mut index_vector: Vec<i32> - vec![];
-		let mut aux: i32 = &nums[0];
-		// задача, изменить вектор, убрать дуплиаты и вернуть значение после
-		// после которого идёт правильный вектор
-		
-		for (index, num) in &mut nums.iter().enumerate(){
-
-		if aux == num{ }
+	pub fn remove_dublicates(nums:&mut Vec<i32>) -> i32{
+		let mut index_vector: Vec<i32> = vec![];
+		let mut aux: i32 = nums[0];
+		for (index, num) in &mut nums[1..].iter().enumerate(){
+			match num{
+				num if *num == aux => index_vector.push(index as i32),
+				_ => aux = *num,
+			}
 		}
+
+		for index_num in index_vector.iter().rev(){
+			&mut nums.remove(*index_num as usize);
+		}
+	 	
+		let answer = &mut nums.len().clone();
+		*answer as i32
 	}
+}
+
+
+
+//reference solution
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut i = 0;
+        let mut j = 0;
+        let mut set: HashSet<i32> = HashSet::<i32>::new();
+
+        while j < nums.len() {
+            match set.insert(nums[j]) {
+                true => {
+                    nums[i] = nums[j];
+                    i += 1;
+                    j += 1;
+                }
+                false => j += 1,
+            };
+        }
+        return i as i32;
+    }
 }
